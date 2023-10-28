3ca762b25e0e381acb9c94444f62e4155c1a2d3b
sebascomeau <sebascomeau@gmail.com>
2023-10-27 22:18:14 -0300

    Install Tailwind CSS

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,16 +1,16 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from '@remix-run/node';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: 'New Remix App' },
+    { name: 'description', content: 'Welcome to Remix!' },
   ];
 };
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1 className="text-3xl font-bold underline">Welcome to Remix</h1>
       <ul>
         <li>
           <a

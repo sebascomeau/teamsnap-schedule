83a7bc420d3467769a20130df31f8f11b20af0ab
sebascomeau <sebascomeau@gmail.com>
2023-10-31 12:28:58 -0300

    add site header

---
+++ app/components/Nav.tsx
@@ -0,0 +1,100 @@
+import * as React from 'react';
+import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { cn } from '~/lib/utils';
+import { Button } from '~/components/ui/button';
+import { ScrollArea } from '~/components/ui/scroll-area';
+import { Sheet, SheetContent, SheetTrigger } from '~/components/ui/sheet';
+import type { LinkProps } from '@remix-run/react';
+import { Link } from '@remix-run/react';
+
+export function MobileNav() {
+  const [open, setOpen] = React.useState(false);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild>
+        <Button
+          variant="ghost"
+          className="mr-2 px-0 text-base hover:bg-transparent focus-visible:bg-transparent focus-visible:ring-0 focus-visible:ring-offset-0"
+        >
+          <HamburgerMenuIcon className="h-5 w-5" />
+          <span className="sr-only">Toggle Menu</span>
+        </Button>
+      </SheetTrigger>
+      <SheetContent side="left" className="pr-0">
+        <MobileLink
+          to="/"
+          className="flex space-x-2 items-center"
+          onOpenChange={setOpen}
+        >
+          <img
+            alt=""
+            src="https://bathurstminorhockey.teamsnapsites.com/wp-content/uploads/sites/415/2023/08/Hockey-mineur-_SSa-R06b_Mil-1-3.png"
+            className="w-10 h-10"
+          />
+          <span className="font-bold">Bathurst Hockey</span>
+        </MobileLink>
+        <ScrollArea className="my-4 h-[calc(100vh-8rem)] pb-10 pl-6">
+          <div className="flex flex-col space-y-2">
+            {[
+              {
+                title: 'Schedule',
+                items: [
+                  { id: '-1', href: '/', title: 'All Teams', disabled: false },
+                ],
+              },
+            ].map((item, index) => (
+              <div key={index} className="flex flex-col space-y-3 pt-6">
+                <h4 className="font-medium">{item.title}</h4>
+                {item.items.length > 0 &&
+                  item.items.map((item) => (
+                    <React.Fragment key={item.href}>
+                      {!item.disabled &&
+                        (item.href ? (
+                          <MobileLink
+                            to={item.href}
+                            onOpenChange={setOpen}
+                            className="text-muted-foreground"
+                          >
+                            {item.title}
+                          </MobileLink>
+                        ) : (
+                          item.title
+                        ))}
+                    </React.Fragment>
+                  ))}
+              </div>
+            ))}
+          </div>
+        </ScrollArea>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+interface MobileLinkProps extends LinkProps {
+  onOpenChange?: (open: boolean) => void;
+  children: React.ReactNode;
+  className?: string;
+}
+
+function MobileLink({
+  to,
+  onOpenChange,
+  className,
+  children,
+  ...props
+}: MobileLinkProps) {
+  return (
+    <Link
+      to={to}
+      onClick={() => {
+        onOpenChange?.(false);
+      }}
+      className={cn(className)}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+}
---
+++ app/components/SiteHeader.tsx
@@ -0,0 +1,20 @@
+import { Link } from '@remix-run/react';
+import { MobileNav } from './Nav';
+
+export const SiteHeader = () => {
+  return (
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 items-center space-x-4 justify-between">
+        <Link to="/" className="flex items-center space-x-2">
+          <img
+            alt=""
+            src="https://bathurstminorhockey.teamsnapsites.com/wp-content/uploads/sites/415/2023/08/Hockey-mineur-_SSa-R06b_Mil-1-3.png"
+            className="w-10 h-10"
+          />
+          <span className="font-bold">Bathurst Hockey</span>
+        </Link>
+        <MobileNav />
+      </div>
+    </header>
+  );
+};

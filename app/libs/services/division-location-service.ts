98465fa6a85719d7af96498c0fc1bcb0b3814d2d
Greg Baker <gregory.j.baker@hrsdc-rhdcc.gc.ca>
2023-11-01 14:26:40 -0230

    Add caching to all fetch requests

--- app/libs/services/division-location-service.ts
+++ app/libs/services/division-location-service.ts
@@ -1,3 +1,8 @@
+import { cachified } from "cachified";
+import {
+  lruCache as cache,
+  defaultTtl as ttl,
+} from "../../libs/utils/cache-utils";
 import type { Config } from "../config";
 import { toDivisionLocationDTO } from "../mappers/division-location-mapper";
 import type { ApiResponse } from "./types";
@@ -7,54 +12,68 @@ export interface DivisionLocationServiceDependencies {
 }
 
 export const DivisionLocationService = (
-  dependencies: DivisionLocationServiceDependencies,
+  dependencies: DivisionLocationServiceDependencies
 ) => {
   const getDivisionLocation = async (id: number) => {
-    const response = await fetch(
-      `https://api.teamsnap.com/v3/division_locations/${id}`,
-      {
-        headers: [
-          ["X-Teamsnap-Client-Id", dependencies.config.TEAMSNAP_CLIENT_ID],
-          ["Accept", "application/vnd.collection+json"],
-        ],
+    return cachified({
+      key: `getDivisionLocation-${id}`,
+      cache,
+      getFreshValue: async () => {
+        const response = await fetch(
+          `https://api.teamsnap.com/v3/division_locations/${id}`,
+          {
+            headers: [
+              ["X-Teamsnap-Client-Id", dependencies.config.TEAMSNAP_CLIENT_ID],
+              ["Accept", "application/vnd.collection+json"],
+            ],
+          }
+        );
+
+        if (!response.ok) {
+          return null;
+        }
+
+        const jsonResponse = (await response.json()) as ApiResponse;
+        return (
+          jsonResponse.collection.items
+            ?.map(({ data }) => toDivisionLocationDTO(data))
+            .find((o) => true) ?? null
+        );
       },
-    );
-
-    if (!response.ok) {
-      return null;
-    }
-
-    const jsonResponse = (await response.json()) as ApiResponse;
-    return (
-      jsonResponse.collection.items
-        ?.map(({ data }) => toDivisionLocationDTO(data))
-        .find((o) => true) ?? null
-    );
+      ttl,
+    });
   };
 
   const searchDivisionLocations = async (query?: { ids?: number[] }) => {
-    const response = await fetch(
-      `https://api.teamsnap.com/v3/division_locations/search?id=${
-        query?.ids?.join(",") ?? ""
-      }`,
-      {
-        headers: [
-          ["X-Teamsnap-Client-Id", dependencies.config.TEAMSNAP_CLIENT_ID],
-          ["Accept", "application/vnd.collection+json"],
-        ],
+    const id = query?.ids?.join(",") ?? "";
+
+    return cachified({
+      key: `searchDivisionLocations-${id}`,
+      cache,
+      getFreshValue: async () => {
+        const response = await fetch(
+          `https://api.teamsnap.com/v3/division_locations/search?id=${id}`,
+          {
+            headers: [
+              ["X-Teamsnap-Client-Id", dependencies.config.TEAMSNAP_CLIENT_ID],
+              ["Accept", "application/vnd.collection+json"],
+            ],
+          }
+        );
+
+        if (!response.ok) {
+          return [];
+        }
+
+        const jsonResponse = (await response.json()) as ApiResponse;
+        return (
+          jsonResponse.collection.items?.map(({ data }) =>
+            toDivisionLocationDTO(data)
+          ) ?? []
+        );
       },
-    );
-
-    if (!response.ok) {
-      return [];
-    }
-
-    const jsonResponse = (await response.json()) as ApiResponse;
-    return (
-      jsonResponse.collection.items?.map(({ data }) =>
-        toDivisionLocationDTO(data),
-      ) ?? []
-    );
+      ttl,
+    });
   };
 
   return {
